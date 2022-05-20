e6b378353e4d6d45ecdc91ffb5465963d01d9f06
SR2168886 <202168886@collegeahuntsic.qc.ca>
2022-05-20 16:34:15 -0400

    Bandes Dessinees

--- src/Biblio/BandeDessinee.java
+++ src/Biblio/BandeDessinee.java
@@ -2,4 +2,30 @@ package Biblio;
 
 public class BandeDessinee extends Volume {
 
+	protected int numEdition;
+
+	public BandeDessinee() {
+		setTitre("Aucun Titre");
+		setAuteur("Aucun Auteur");
+		setNumEdition(0);
+	}
+
+	public BandeDessinee(String Titre, String auteur, int numEdition) {
+		setTitre(titre);
+		setAuteur(auteur);
+		setNumEdition(numEdition);
+	}
+
+	public int getNumEdition() {
+		return numEdition;
+	}
+
+	public void setNumEdition(int numEdition) {
+		this.numEdition = numEdition;
+	}
+
+	@Override
+	public String toString() {
+		return code + ",\t" + titre + ",\t" + auteur + ",\t" + numEdition;
+	}
 }
