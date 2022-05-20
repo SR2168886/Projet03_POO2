39ee707e329209633e883b7a7847a17f362577ff
SR2168886 <202168886@collegeahuntsic.qc.ca>
2022-05-20 16:16:07 -0400

    why did you even put accents in code

--- src/Biblio/Livre.java
+++ src/Biblio/Livre.java
@@ -2,18 +2,15 @@ package Biblio;
 
 public class Livre extends Volume {
 
-
-
 	private int annee;
 
 	private String genre;
-
 
 	public Livre() {
 
 	}
 
-	public Livre(String auteur ,String titre,int annee,String genre, int nbcopie ) {
+	public Livre(String auteur, String titre, int annee, String genre, int nbcopie) {
 		setAuteur(auteur);
 		setTitre(titre);
 		setAnnee(annee);
@@ -23,36 +20,27 @@ public class Livre extends Volume {
 
 	}
 
-
-
-
-
 	public String getGenre() {
 		return genre;
 	}
+
 	public void setGenre(String genre) {
 		this.genre = genre;
 	}
-
-
 
 	public int getAnnee() {
 		return annee;
 	}
 
-
-
-
-	public void setAnnee(int année) {
-		this.annee = année;
+	public void setAnnee(int annee) {
+		this.annee = annee;
 	}
-
 
 	@Override
 	public String toString() {
-		String message = "Livre"+ "\t"+ getCode()+ "\t"+getTitre() + "\t"+getAuteur()+"\t"+ genre + "\t" +getNbCopie()+"\t"+ getNbCopieDispo();
+		String message = "Livre" + "\t" + getCode() + "\t" + getTitre() + "\t" + getAuteur() + "\t" + genre + "\t"
+				+ getNbCopie() + "\t" + getNbCopieDispo();
 		return message;
 	}
-
 
 }
