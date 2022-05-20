e4451831ea03eb73dd2065568a4a812713bc3041
SR2168886 <202168886@collegeahuntsic.qc.ca>
2022-05-20 15:01:48 -0400

    e

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+/*
+ * Fichier : Test.java
+ * Auteur : Sébastien
+ * Date de Creation : May 13, 2022
+ */
+
+/**
+ * TODO Auteur : Sebastien Racicot
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
